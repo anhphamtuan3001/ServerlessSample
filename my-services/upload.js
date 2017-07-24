ede3531b0f66073bf70cfbb3de8448e251c74a06
NaoshiHoshi <hoshinaoshi@gmail.com>
2017-07-25 07:46:16 +0900

    eslint結果反映

--- my-services/upload.js
+++ my-services/upload.js
@@ -1,44 +1,44 @@
 'use strict';
-const aws = require("aws-sdk");
+const aws = require('aws-sdk');
 aws.config.region = 'us-west-2';
 
 module.exports.handle = (event, context, callback) => {
   const json = JSON.parse(JSON.stringify(event, undefined, 1));
   console.log(json);
   console.log(JSON.stringify(context, undefined, 1));
+  
+  const buffer = new Buffer(json.body.replace(/^data.+,/,''), 'base64');
+  const params = {
+    Key:         json.headers.file_name,
+    ContentType: 'image/jpg',
+    Body:        buffer
+  };
+  const options = {
+    params: {
+      apiVersion: '2006-03-01',
+      Bucket:     'sample-uploads'
+    }
+  };
+
+  var bucket = new aws.S3(options);
+  var statusCode = 200;
+  var resultMsg  = 'Upload success!!';
+  bucket.putObject(params, function(err, data) {
+    if (err) {
+      statusCode = err.statusCode;
+      resultMsg  = err.message;
+      console.log(err, err.stack);
+    } else {
+      console.log(data);
+    }
+  });
+
   const response = {
-    statusCode: 200,
+    statusCode: statusCode,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: resultMsg,
       input: event,
     }),
   };
-  const buffer = new Buffer(json.body.replace(/^data.+,/,""), 'base64'); // ファイルのbase64データ以外の情報を消す。
-  var params = {
-    Key: json.headers.file_name,
-    ContentType: "image/jpg",
-    Body: buffer
-  };
-  var options = {
-    params: {
-      apiVersion: '2006-03-01',
-      Bucket: "sample-uploads"
-    }
-  };
-  var bucket = new aws.S3(options);
-  bucket.putObject(params, function(err, data) {
-    if (err) {
-      //resultMsg = "Error uploading data";
-      //statusCode = 500;
-      //console.log(resultMsg, err);
-    } else {
-      //console.log(resultMsg, data);
-    }
-    context.done(null, 'Finished UploadObjectOnS3');
-  });
-
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
