bb9ee9cd70ec1b2c18ca5a53d725fb601da0c48a
NaoshiHoshi <hoshinaoshi@gmail.com>
2017-07-25 07:54:59 +0900

    eslint結果反映

--- my-services/after_upload.js
+++ my-services/after_upload.js
@@ -1,5 +1,5 @@
 'use strict';
-const aws = require("aws-sdk");
+const aws = require('aws-sdk');
 aws.config.region = 'us-west-2';
 const dynamoDB = new aws.DynamoDB({region: 'us-west-2'});
 
@@ -7,28 +7,35 @@ module.exports.store_metadata_for_dynamo = (event, context, callback) => {
   console.log(JSON.stringify(event, undefined, 1));
   console.log(JSON.stringify(context, undefined, 1));
   
-  event.Records.forEach(function(record){
-    var params = {
+  event.Records.forEach(function(record) {
+    const params = {
       TableName: 'sample-items',
       Item: {
-        'bucket':      { "S": record.s3.bucket.name },
-        'file_name':   { "S": record.s3.object.key },
-        'size':        { "N": record.s3.object.size.toString() },
-        'uploaded_at': { "N": (new Date()).getTime().toString() }
+        'bucket':      { 'S': record.s3.bucket.name },
+        'file_name':   { 'S': record.s3.object.key },
+        'size':        { 'N': record.s3.object.size.toString() },
+        'uploaded_at': { 'N': (new Date()).getTime().toString() }
       }
     };
     dynamoDB.putItem(params, function (err, data) {
       if (err) {
         console.log(err, err.stack);
+        callback(null, generateResponse(event, err.statusCode, err.message));
       } else {
         console.log(data);
+        callback(null, generateResponse(event, 200, 'Upload Success!!'));
       }
     });
   });
-
-  const response = {
-    statusCode: 200,
-    body: "{\"status\": \"seikou\"}"
-  };
-  callback(null, response);
 };
+
+function generateResponse(event, statusCode, resultMsg) {
+  const response = {
+    statusCode: statusCode,
+    body: JSON.stringify({
+      message: resultMsg,
+      input: event,
+    }),
+  };
+  return response;
+}
